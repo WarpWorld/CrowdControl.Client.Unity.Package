65ec4143048a3280461d2066c81319c60cca29ef
Lexi Rose <north@tinymagnet.com>
2026-03-31 22:33:09 -0500

    wip deploy

--- Editors/UnityEffectBaseEditor.cs
+++ Editors/UnityEffectBaseEditor.cs
@@ -1,7 +1,6 @@
 ﻿#if UNITY_EDITOR
 using CrowdControl.Common;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
@@ -12,41 +11,39 @@ namespace CrowdControl.Client.Unity.Editors
     [CustomEditor(typeof(UnityEffectBase), true)]
     public class UnityEffectBaseEditor : Editor
     {
-        private SynchronizationContext? m_context;
-
-        void OnEnable() => m_context = SynchronizationContext.Current;
-
         /// <summary>Custom inspector GUI that adds buttons to test each effect ID during play mode.</summary>
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
 
-            if (Application.isPlaying)
+            if (!Application.isPlaying)
+                return;
+
+            UnityEffectBase effect = (UnityEffectBase)target;
+            string effectID = effect.EffectID;
+
+            if (GUILayout.Button("Test " + effectID))
             {
-                UnityEffectBase effect = (UnityEffectBase)target;
-                string effectID = effect.EffectID;
-                if (GUILayout.Button("Test " + effectID))
+                EffectRequest request = new(effectID);
+
+                if (effect.IsTimed)
                 {
-                    EffectRequest request = new(effectID);
-                    if (effect.IsTimed)
+                    Log.Debug("Starting timed effect: " + effectID);
+                    effect.StartEffect(request);
+
+                    // Schedule stop safely without blocking GUI or relying on SynchronizationContext
+                    Task.Run(async () =>
                     {
-                        Log.Debug("Starting timed effect: " + effectID);
-                        effect.StartEffect(request);
-                        StopEffectAfterDelay(effect, request).Forget();
-                    }
-                    else
-                    {
-                        Log.Debug("Starting instant effect: " + effectID);
-                        effect.StartEffect(request);
-                    }
+                        await Task.Delay((TimeSpan)request.Duration);
+                        EditorApplication.delayCall += () => effect.StopEffect(request);
+                    });
+                }
+                else
+                {
+                    Log.Debug("Starting instant effect: " + effectID);
+                    effect.StartEffect(request);
                 }
             }
-        }
-
-        private async Task StopEffectAfterDelay(UnityEffectBase effect, EffectRequest request)
-        {
-            await Task.Delay((TimeSpan)request.Duration);
-            m_context?.Post(_ => effect.StopEffect(request), null);
         }
     }
 }
