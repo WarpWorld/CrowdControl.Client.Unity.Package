4f35b569634bd92015dc87103c3ec7dd4dc59249
Lexi Rose <north@tinymagnet.com>
2026-03-31 20:17:30 -0500

    wip deploy

--- Runtime/UnityMainThreadTaskScheduler.cs
+++ Runtime/UnityMainThreadTaskScheduler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,12 +8,30 @@ namespace CrowdControl.Client.Unity
 {
     public sealed class UnityMainThreadTaskScheduler : TaskScheduler
     {
-        private readonly ConcurrentQueue<Task> _tasks = new();
-        private SynchronizationContext m_synchronizationContext;
+        private readonly SynchronizationContext m_synchronizationContext;
+        private readonly ConcurrentQueue<Task> m_scheduledTasks = new ConcurrentQueue<Task>();
 
-        public UnityMainThreadTaskScheduler(SynchronizationContext synchronizationContext) => m_synchronizationContext = synchronizationContext;
+        public UnityMainThreadTaskScheduler(SynchronizationContext synchronizationContext)
+            => m_synchronizationContext = synchronizationContext ?? throw new ArgumentNullException(nameof(synchronizationContext));
 
-        protected override void QueueTask(Task task) => _tasks.Enqueue(task);
+        protected override void QueueTask(Task task)
+        {
+            m_scheduledTasks.Enqueue(task);
+
+            m_synchronizationContext.Post(static state =>
+            {
+                var (scheduler, t) = ((UnityMainThreadTaskScheduler, Task))state!;
+                scheduler.ExecuteQueuedTask(t);
+            }, (this, task));
+        }
+
+        private void ExecuteQueuedTask(Task task)
+        {
+            if (m_scheduledTasks.TryDequeue(out Task? dequeued) && !ReferenceEquals(dequeued, task))
+                m_scheduledTasks.Enqueue(dequeued);
+
+            TryExecuteTask(task);
+        }
 
         protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
         {
@@ -22,27 +41,6 @@ namespace CrowdControl.Client.Unity
             return TryExecuteTask(task);
         }
 
-        protected override IEnumerable<Task>? GetScheduledTasks() => _tasks.ToArray();
-
-        public bool ExecuteOne()
-        {
-            if (_tasks.TryDequeue(out Task task))
-                return TryExecuteTask(task);
-
-            return false;
-        }
-
-        public int ExecuteAll()
-        {
-            int count = 0;
-
-            while (_tasks.TryDequeue(out Task task))
-            {
-                if (TryExecuteTask(task))
-                    count++;
-            }
-
-            return count;
-        }
+        protected override IEnumerable<Task> GetScheduledTasks() => m_scheduledTasks.ToArray();
     }
 }
