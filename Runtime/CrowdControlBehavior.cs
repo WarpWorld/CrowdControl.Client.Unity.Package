d3df9180f126472477660313e0b3904ba0b69192
Lexi Rose <north@tinymagnet.com>
2026-04-03 14:36:00 -0500

    wip il2cpp fixes

--- Runtime/CrowdControlBehavior.cs
+++ Runtime/CrowdControlBehavior.cs
@@ -5,7 +5,6 @@ using CrowdControl.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
