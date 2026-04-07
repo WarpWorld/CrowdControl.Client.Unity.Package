a10caceb3569b15aa2687b5573a1b5b83753ffe1
Lexi Rose <north@tinymagnet.com>
2026-04-07 12:39:52 -0500

    il2cpp-compatibility wip deploy

--- Runtime/UnityEffectBase.cs
+++ Runtime/UnityEffectBase.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using CrowdControl.Client.WebSocket.Actions;
 using CrowdControl.Common;
 using UnityEngine;
