cfe8d7c008cbcebf3db0e5b7e17b1240829f3a1f
Lexi Rose <north@tinymagnet.com>
2026-04-27 16:58:49 -0500

    wip deploy

--- Editor/UnityEffectBaseEditor.cs
+++ Editor/UnityEffectBaseEditor.cs
@@ -9,6 +9,8 @@ namespace CrowdControl.Client.Unity.Editor
     [CustomEditor(typeof(UnityEffectBase), true)]
     public class UnityEffectBaseEditor : UnityEditor.Editor
     {
+        private int testQuantity = 1;
+
         /// <summary>Custom inspector GUI that adds buttons to test each effect ID during play mode.</summary>
         public override void OnInspectorGUI()
         {
@@ -19,10 +21,20 @@ namespace CrowdControl.Client.Unity.Editor
 
             UnityEffectBase effect = (UnityEffectBase)target;
             string effectID = effect.EffectID;
+            uint maxQuantity = effect.MaxQuantity;
+            int maxTestQuantity = maxQuantity > int.MaxValue ? int.MaxValue : (int)maxQuantity;
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Testing", EditorStyles.boldLabel);
+
+            testQuantity = EditorGUILayout.IntSlider("Quantity", Mathf.Clamp(testQuantity, 1, maxTestQuantity), 1, maxTestQuantity);
 
             if (GUILayout.Button("Test " + effectID))
             {
+                uint quantity = (uint)testQuantity;
+
                 EffectRequest request = new(effectID);
+                request.Quantity = quantity;
 
                 if (effect.IsTimed)
                 {
