b6d4aa6479d11311ec83d2fc941f157b531bce90
Lexi Rose <north@tinymagnet.com>
2026-05-26 11:54:58 -0500

    wip deploy

--- Runtime/UnityEffectBase.cs
+++ Runtime/UnityEffectBase.cs
@@ -2,7 +2,6 @@ using CrowdControl.Client.WebSocket.Actions;
 using CrowdControl.Common;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
